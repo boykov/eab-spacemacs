7e8474082bff3fa35c4be84ff7fcb2b8bc2d67db
Evgeny Boykov <artscan@list.ru>
2015-04-27 22:05:04 +1000

    first commit

---
+++ extensions.el
@@ -0,0 +1,48 @@
+;;; extensions.el --- eab Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar eab-pre-extensions
+  '(
+    eab-misc
+    ;; pre extension eabs go here
+    )
+  "List of all extensions to load before the packages.")
+
+(defvar eab-post-extensions
+  '(
+    eab-dotemacs
+    ;; post extension eabs go here
+    )
+  "List of all extensions to load after the packages.")
+
+
+(defun eab/init-eab-misc ()
+  ;; (add-to-list 'load-path (concat user-emacs-directory "private/eab/extensions/eab-misc"))
+  (setq dotfiles-dir user-emacs-directory)
+  (setq dotemacs-children-prefix dotfiles-dir)
+  (load (concat dotfiles-dir "private/eab/extensions/eab-misc/dotemacs.el"))
+  )
+
+(defun eab/init-eab-dotemacs ()
+  (dotemacs-load-children '("private/eab/extensions/eab-dotemacs/eab-packages-minimal"))
+  (dotemacs-load-children '("private/eab/extensions/eab-dotemacs/eab-kbd")))
+
+
+;; For each extension, define a function eab/init-<extension-eab>
+;;
+;; (defun eab/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
