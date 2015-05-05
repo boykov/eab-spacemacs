91b7b9de958f3b31b599fb4146823566ced818c5
Evgeny Boykov <artscan@list.ru>
2015-05-05 17:28:46 +1000

    delete dotemacs.el

--- extensions.el
+++ extensions.el
@@ -13,29 +13,23 @@
 (defvar eab-spacemacs-pre-extensions
   '(
     eab-misc
+    eab-dotemacs
     ;; pre extension eabs go here
     )
   "List of all extensions to load before the packages.")
 
 (defvar eab-spacemacs-post-extensions
   '(
-    eab-dotemacs
     ;; post extension eabs go here
     )
   "List of all extensions to load after the packages.")
 
 
 (defun eab-spacemacs/init-eab-misc ()
-  ;; (add-to-list 'load-path (concat user-emacs-directory "private/eab/extensions/eab-misc"))
-  (setq dotfiles-dir user-emacs-directory)
-  (setq dotemacs-children-prefix dotfiles-dir)
-  (load (concat dotfiles-dir "private/eab-spacemacs/extensions/eab-misc/dotemacs.el"))
   )
 
 (defun eab-spacemacs/init-eab-dotemacs ()
-  (dotemacs-load-children '("private/eab-spacemacs/extensions/eab-dotemacs/eab-packages-minimal"))
-  (dotemacs-load-children '("private/eab-spacemacs/extensions/eab-dotemacs/eab-kbd")))
-
+  (use-package eab-kbd))
 
 ;; For each extension, define a function eab/init-<extension-eab>
 ;;
