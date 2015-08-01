bc9e9e460922d819339d03267083cf099fefeca9
Evgeny Boykov <artscan@list.ru>
2015-08-01 22:14:22 +1000

    add eab-path to config.el, add packages

--- config.el
+++ config.el
@@ -14,6 +14,8 @@
 ;; Prefixes
 ;; ---------------------------------------------------------------------------
 
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 (defun eab/loaded-ok ()
   (if (not configuration-layer-error-count)
       (kill-emacs)
@@ -26,3 +28,184 @@
   (message (with-current-buffer (get-buffer-create "*spacemacs*")
 	     (buffer-substring-no-properties (point-min) (point-max))))
   (message "Spacemacs is ready."))
+
+(setq eab/homedir (getenv "HOME"))
+(setq eab/newemacs-path "/home/eab/newemacs")
+(setq eab/daemon-name (daemonp))
+
+(if (not (stringp eab/daemon-name))
+    (setq eab/daemon-name "server"))
+
+(defun eab/onhomedir (def)
+  (if (string= eab/homedir def) 't))
+
+(defun eab/ondaemon (def)
+  (if (string= eab/daemon-name def) 't))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar eab/paths-hash nil "symbol : value")
+
+(setq eab/paths-hash (make-hash-table :test 'equal))
+
+(defmacro setq-put (name value)
+  `(puthash ',name ,value eab/paths-hash))
+
+(defun eab/get-path (name)
+  (gethash name eab/paths-hash))
+
+(defmacro eab/bind-path (name)
+  `(setq ,name (gethash ',name eab/paths-hash)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun eab/read-lines (file)
+  "Return a list of lines of a file at at FPATH."
+  (if (file-exists-p file)
+      (with-temp-buffer
+	(insert-file-contents file)
+	(split-string (buffer-string) "\n" t))))
+
+(defun eab/onhost (def)
+  (if (string= system-name def) 't))
+
+(defun eab/history-dir ()
+  (let ((dir
+	 (eval (cdr
+		(assoc eab/daemon-name (gethash 'eab/history-dir-alist eab/paths-hash))))))
+    (if (not (file-exists-p dir))
+	(ignore-errors (make-directory dir)))
+    dir))
+
+(defun eab/desktop-dir ()
+  (eab/history-dir))
+
+(setq-put eab/history-dir-alist
+	  '(
+	    ("ea2b" . (concat user-emacs-directory "history2/"))
+	    ("ghost" . (concat user-emacs-directory "history/"))
+	    ("server" . (concat user-emacs-directory "history/"))
+	    ("serverN" . (concat user-emacs-directory "historyN/"))
+	    ("serverM" . (concat user-emacs-directory "historyM/"))
+	    ("serverP" . (concat user-emacs-directory "historyP/"))
+	    ))
+
+
+;; TODO можно ли подобные настройки не считать "путями" и убрать из path?
+(setq-put org-clock-persist-file (concat (eab/history-dir) "org-clock-save.el"))
+(setq-put org-id-locations-file (concat (eab/history-dir) ".org-id-locations"))
+
+(setq-put org-directory "~/git/org/")
+(setq-put eab/org-publish-directory "~/pub/org/")
+(setq-put eab/org-publish-directory-file "file:///home/eab/pub/org/")
+(setq-put org-mobile-directory "~/Dropbox/MobileOrg")
+(setq-put org-ditaa-jar-path "~/bin/ditaa0_9.jar")
+
+(setq-put org-link-abbrev-alist
+	  '(("bib" . "~/git/lit/boykov.bib::%s")
+	    ("papers" . "~/pub/papers/%s.pdf")
+	    ("google" . "http://www.google.com/search?q=")
+	    ))
+;; See also eab-header in ~/texmf/tex/latex/eab-styles/eab-header.sty
+
+
+;; (eab/print-0 (wg-workgroup-names))
+;; (mapcar 'wg-create-workgroup '(":aaaa:" ":ag:" ":ahmed:" ":auto:" ":bempp:" ":bie:" ":dbsym:" ":dot:" ":doteab:" ":draft:" ":eabdot:" ":gsie:" ":jac:" ":lat:" ":site:" ":testbed:" ":tmp:" "asynk" "bib" "boy" "path"))
+
+(setq-put eab/wg-update-list
+	  '(("~/.emacs.d/" ":dot:")
+	    ("~/git/dotemacs/" ":dot:")
+	    ("~/git/dotemacs/lisp/" ":dot:")
+	    ("~/.emacs.d/lisp/" ":dot:")
+	    ("~/git/org/" ":ag:")
+	    ("~/git/org/clock/" ":ag:")
+	    ("~/git/difwave/" ":gsie:")
+	    ("~/git/cc/" ":lat:")))
+
+(setq-put eab/workgroups-save (concat (eab/history-dir) ".emacs_workgroups"))
+(setq-put wg-session-file (eab/bind-path eab/workgroups-save))
+
+
+(setq-put ac-comphist-file (concat (eab/history-dir) "ac-comphist.dat"))
+(setq-put save-place-file (concat (eab/history-dir) ".emacs-places"))
+;; (setq-put eab/eab-workgroups (concat (eab/history-dir) "eab-workgroups"))
+(setq-put savehist-file (concat (eab/history-dir) "history"))
+(setq-put smex-save-file (concat (eab/history-dir) ".smex-items"))
+(setq-put eab/trans-path "~/git/python/trans.py")
+(setq-put eab/translate-path "~/bin/translate")
+(setq-put eab/print-dir "~/pub/print/")
+(setq-put eab/tmp-ps "/tmp/tmp.ps")
+
+(setq-put eab/eeansi-path "/home/eab/git/auto/eeansi.sh")
+;; (setq-put eab/eegchannel-path (expand-file-name (concat user-emacs-directory "el-get/eev-current/eegchannel")))
+(setq-put eab/eegchannel-path (expand-file-name (concat user-emacs-directory "private/eab-spacemacs/extensions/eev-current/eegchannel")))
+
+
+(setq-put abbrev-file-name (concat (eab/history-dir) ".abbrev_defs"))
+(setq-put ido-save-directory-list-file (concat (eab/history-dir) ".ido.last"))
+(setq-put mc/list-file (concat (eab/history-dir) ".mc-lists.el"))
+(setq-put pm-macro-files `(,(concat user-emacs-directory "eab-pmacros.el")))
+(setq-put power-macros-file (concat user-emacs-directory "eab-pmacros.el"))
+(setq-put reftex-default-bibliography '("~/git/lit/boykov.bib"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(cond ((eab/onhost "jonesbook") (setq-put source-directory "~/emacs/src/emacs/")) ;; DONE old path
+      ((eab/onhost "victory")   (setq-put source-directory "~/src/emacs/"))
+      ((eab/onhost "kairos")   (setq-put source-directory "~/data/gitno/github/emacs/src")))
+
+
+(setq-put custom-file (concat user-emacs-directory "custom.el"))
+(setq-put eab/emaxima-path (concat user-emacs-directory "private/eab-spacemacs/extensions/eab-misc/emaxima"))
+
+(setq-put auto-save-list-file-prefix (concat (eab/history-dir) "auto-save-list/.saves-"))
+
+;; TODO auto-save и торможение при C-s
+(defconst emacs-tmp-dir
+  (format "%s/%s%s/"
+	  (concat (eab/history-dir) "backup")
+	  (concat "emacs" eab/daemon-name)
+	  (user-uid)))
+
+(ignore-errors (make-directory emacs-tmp-dir))
+
+(setq-put backup-directory-alist `((".*" . ,emacs-tmp-dir)))
+(setq-put auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t)))
+
+(setq-put keyfreq-file (concat (eab/history-dir) ".emacs.keyfreq"))
+
+(setq-put eab/check-cc-path "~/git/auto/check-cc.sh") ;; see private/eab-secrets.el.gpg
+(setq-put eab/maple-path "~/data/maple16/bin/maple")
+(setq-put eab/maxima-path "maxima")
+(setq-put eab/ipython-path "/usr/local/bin/ipython")
+(setq-put eab/check-inet-path "~/git/auto/check-inet.sh")
+(setq-put eab/musicdb-path "~/data/dbs/media/music.db")
+(setq-put eab/downloads-path "~/downloads/")
+(setq-put eab/twittering-mode (expand-file-name "~/.twittering-mode.gpg"))
+(setq-put eab/twittering-modeN (expand-file-name "~/.twittering-modeN.gpg"))
+
+(setq-put eab/american-english (eab/read-lines "/usr/share/dict/american-english"))
+
+(setq-put achievements-file (concat (eab/history-dir) ".achievements"))
+(setq-put auto-install-directory (concat user-emacs-directory "auto-install/"))
+(setq-put bbdb-file (concat user-emacs-directory "private/.bbdb"))
+(setq-put bibtex-files '("~/git/lit/boykov.bib"))
+(setq-put bmkp-last-as-first-bookmark-file (concat (eab/history-dir) ".emacs.bmk"))
+(setq-put bookmark-default-file (concat (eab/history-dir) ".emacs.bmk"))
+(setq-put ebib-file-search-dirs '("~/git/lit/"))
+(setq-put ebib-preload-bib-files '("~/git/lit/boykov.bib"))
+(setq-put erc-log-channels-directory (concat (eab/history-dir) "logs/"))
+(setq-put eshell-history-file-name "~/.bash_history")
+(setq-put gnus-init-file (concat user-emacs-directory ".gnus"))
+(setq-put gnus-startup-file (concat (eab/history-dir) ".newsrc"))
+(setq-put helm-c-adaptative-history-file (concat (eab/history-dir) "helm-adaptive-history"))
+(setq-put nnmail-message-id-cache-file (concat (eab/history-dir) ".nnmail-cache"))
+(setq-put tramp-persistency-file-name (concat (eab/history-dir) "tramp"))
+(setq-put url-configuration-directory (concat (eab/history-dir) "url/"))
+
+(setq-put eab/yasnippets-path (concat user-emacs-directory "private/eab-spacemacs/extensions/yasnippet-snippets"))
+(setq-put eab/eab-snippets-path (concat user-emacs-directory "snippets"))
+
+;; DONE похоже, из-за bbdb-loaddefs подгружается еще один путь,
+;; который отличается в символе /. А потом еще и auctex аналогично.
+;; DONE какой-то из доп. пакетов el-get в eab-packages ломает подсветку: оказалось, это дело в 8.0.3 версии org-mode
--- packages.el
+++ packages.el
@@ -10,18 +10,176 @@
 ;;
 ;;; License: GPLv3
 
-(load "/home/eab/data/gitno/github/dotspace/eab-spacemacs/extensions/eab-dotemacs/eab-packages-minimal.el")
-
-(load "/home/eab/data/gitno/github/dotspace/eab-spacemacs/extensions/eab-dotemacs/eab-packages.el")
-
-(load "/home/eab/.emacs.d/eab-path.el")
-
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
 (defvar eab-spacemacs-packages
-  (append
-   eab/package-sources-minimal
-   eab/package-sources-extra)
+  '(
+  spacemacs-theme
+  ergoemacs-mode
+  key-chord
+  auto-dictionary ;; switcher for flyspell
+  auto-complete ;; depend
+  ;; auto-complete-emacs-lisp ;; no melpa depend auto-complete
+  howdoi
+  ac-dabbrev
+  etags-table
+  etags-select
+  helm
+  helm-descbinds
+  smart-compile
+  popwin
+  moz
+  expand-region
+  multiple-cursors
+  flx-isearch
+  workgroups2
+  dictionary
+  sauron
+  magit
+  magit-filenotify ;; needs emacs 24.4 with file-notify-support
+  git-commit
+  ;;	git-rebase ;; see in magit
+  magit-annex
+  orgit ;; org-magit obsolete
+  auctex
+  org-agenda-property
+  region-bindings-mode
+  smex
+  smartparens
+  auto-install
+  projectile
+  flx-ido
+  ido-at-point
+  paredit
+  grep-a-lot
+  wgrep
+  undo-tree
+  ;; workgroups
+  edit-list
+  yasnippet
+
+  docker
+  dired+
+  dired-details
+  dired-details+
+  elpa-mirror
+  smex
+  undo-tree
+  yasnippet
+  region-bindings-mode
+  smartparens
+  projectile
+  ace-window
+  ace-jump-buffer
+  ace-link
+  twittering-mode
+  request
+  python-mode
+  pydoc-info
+  popup
+  idle-highlight-mode ;; + no melpa
+  help+
+  help-fns+
+  help-mode+
+  fuzzy
+  el-mock
+  anchored-transpose
+  bookmark+
+  buffer-move
+  crontab-mode
+  shut-up
+  parsebib
+  package-build
+  jedi-core
+  ebib
+  helm-helm-commands
+  dockerfile-mode
+  deft
+  ewmctrl
+  anaphora
+  connection
+  ido-vertical-mode
+  link
+  oneonone
+  emacsc
+  deferred ;; depend
+  paredit
+  async ;; another name: emacs-async
+  org-agenda-property
+  ido-at-point
+  web-server
+  take-off
+  restclient
+  wide-n
+  god-mode
+  fancy-narrow
+  popup
+  outshine
+  outorg
+  goto-chg
+  epc
+  ctable
+  concurrent
+  auto-complete
+  python-environment
+  deferred
+  jedi
+  ;; ipython
+  bibretrieve
+  git-annex
+  websocket
+  org-jekyll
+  pkg-info
+  epl
+  s
+  python-info
+  achievements
+  org-grep
+  org
+  f
+  keyfreq
+  cask ;; shut-up
+  pallet ;; shut-up
+  purty-mode
+  flx
+  guide-key
+  popwin
+  xml-rpc
+  web
+  string-edit
+  smart-operator
+  redo+
+  pcache
+  org-plus-contrib
+  org-ehtml
+  noflet
+  minimap
+  markdown-mode
+  logito
+  kv
+  jira
+  ignoramus
+  ibuffer-vc
+  highlight
+  graphviz-dot-mode
+  gist  ;; tabulated-list
+  gh
+  fuzzy-match
+  feature-mode
+  fakir ;; kv
+  elnode ;; kv
+  el-x
+  diminish
+  ;; descbinds-anything
+  db
+  dash
+  creole ;; kv
+  clojure-mode
+  browse-kill-ring
+  bm
+  ;; anything-obsolete
+  ;; anything-ipython
+  ;; anything-extension
+  ;; anything-complete
+  )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
