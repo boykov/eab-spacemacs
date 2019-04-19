c058adeb87ddff6c412960c5c29fc236b17ca800
Evgeny Boykov <artscan@list.ru>
2019-04-19 21:20:04 +1000

    improve eab/kbd-macro-query: recursive edit macro execution C-o C-n

--- lisp/eab-minimal.el
+++ lisp/eab-minimal.el
@@ -15,6 +15,22 @@
      (interactive)
      ,@body))
 
+(defmacro eab/do-action (&rest body)
+  ""
+  `(lambda ()
+     (interactive)
+     (let ((executing-kbd-macro defining-kbd-macro))
+       (run-with-timer 0.1 nil ,@body)
+       (abort-recursive-edit))))
+
+(defun eab/kbd-macro-query ()
+  (interactive)
+  (let (executing-kbd-macro defining-kbd-macro)
+    (run-with-timer 0.1 nil (ilam (execute-kbd-macro (read-kbd-macro "C-g"))))
+    (run-with-timer 0.2 nil (ilam (execute-kbd-macro (read-kbd-macro "M-g"))
+				  (setq kill-ring (cdr kill-ring))))
+    (recursive-edit)))
+
 (defmacro eab/add-hook (hookname funcname &rest body)
   "add-hook with lambda progn"
   (declare (indent defun))
